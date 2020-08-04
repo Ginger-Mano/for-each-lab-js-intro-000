876eaecebc5c4f758e19ace4a7afc339d505da90
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-08-04 23:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ array.forEach((element, index) => {
   console.log(`${index}: ${element}`)
 })
 }
+
+function iterate(callback) {
+  const livingThings = ['cats', 'dogs', 'people', 'plants'];
+  livingThings.forEach((element) => {
+    console.log(livingThings)
+  })
+}
